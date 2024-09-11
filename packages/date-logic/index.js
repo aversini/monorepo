dde56bf737a9ea3be2fab972b053ca893e8118a8
Arno V <versini@gmail.com>
2024-09-11 18:36:55 -0400

    feat(date-logic): default month is now short

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -3,7 +3,7 @@
  */
 module.exports = function getDate(
   locale = "en-US",
-  options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
+  options = { weekday: "long", year: "numeric", month: "short", day: "numeric" }
 ) {
   return new Date().toLocaleDateString(locale, options);
 };
